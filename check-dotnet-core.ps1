52a7b9b1bdcc6ba7a482e7834e548983a4a20724
Feodor Fitsner <feodor@appveyor.com>
2016-09-18 17:12:29 -0700

    Added messages to test script

--- check-dotnet-core.ps1
+++ check-dotnet-core.ps1
@@ -1,6 +1,11 @@
 $core_version = (dotnet --version)
+Write-Host "Installed .NET Core: $core_version"
 if($core_version -ne $env:dotnetcore_version) { throw "Expected .NET Core: $env:dotnetcore_version, actual: $core_version"; }
+
+Write-Host "Creating new app to see if .NET Core runtime pre-cached"
+Write-Host "md myapp; cd myapp; dotnet new"
 md myapp; cd myapp
 $core_new_app = (dotnet new)
+$core_new_app
 if($core_new_app -ne "Created new C# project in $env:appveyor_build_folder\myapp.") { throw '.NET Core installation must be pre-cached.'; }
 cd..
