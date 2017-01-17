de5cdf14caf0881e9c9adc0493f724b7b641f457
Feodor Fitsner <feodor@appveyor.com>
2017-01-16 17:42:08 -0800

    new testing apps added

--- check-dotnet-core.ps1
+++ check-dotnet-core.ps1
@@ -9,3 +9,17 @@ $core_new_app = (dotnet new)
 $core_new_app
 if($core_new_app -ne "Created new C# project in $env:appveyor_build_folder\myapp.") { throw '.NET Core installation must be pre-cached.'; }
 cd..
+
+$netCoreRuntime = (Get-ChildItem 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App')
+if($netCoreRuntime.Length -ne 4) { throw 'Wrong number of .NET Core runtimes installed' }
+if($netCoreRuntime[0].Name -ne '1.0.0') { throw '.NET Core 1.0.0 not found'}
+if($netCoreRuntime[1].Name -ne '1.0.1') { throw '.NET Core 1.0.1 not found'}
+if($netCoreRuntime[2].Name -ne '1.0.3') { throw '.NET Core 1.0.3 not found'}
+if($netCoreRuntime[3].Name -ne '1.1.0') { throw '.NET Core 1.1.0 not found'}
+
+$netCoreSDK = (Get-ChildItem 'C:\Program Files\dotnet\sdk')
+if($netCoreSDK.Length -ne 4) { throw 'Wrong number of .NET Core SDKs installed' }
+if($netCoreSDK[0].Name -ne '1.0.0-preview2-003121') { throw '.NET Core SDK 1.0.0-preview2-003121 not found'}
+if($netCoreSDK[1].Name -ne '1.0.0-preview2-003131') { throw '.NET Core SDK 1.0.0-preview2-003131 not found'}
+if($netCoreSDK[2].Name -ne '1.0.0-preview2-003156') { throw '.NET Core SDK 1.0.0-preview2-003156 not found'}
+if($netCoreSDK[3].Name -ne '1.0.0-preview2-1-003177') { throw '.NET Core SDK 1.0.0-preview2-1-003177 not found'}
---
+++ test.cmd
@@ -0,0 +1,23 @@
+cd dotnet-core-1.0.0
+dotnet restore
+dotnet build
+dotnet test
+cd..
+
+cd dotnet-core-1.0.1
+dotnet restore
+dotnet build
+dotnet test
+cd..
+
+cd dotnet-core-1.0.3
+dotnet restore
+dotnet build
+dotnet test
+cd..
+
+cd dotnet-core-1.1.0
+dotnet restore
+dotnet build
+dotnet test
+cd..
