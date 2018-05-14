aa8b952de1eb52cbbfb5122222903e70ceeab5ff
Feodor Fitsner <feodor@appveyor.com>
2018-05-13 21:36:12 -0700

    .NET Core SDK 2.1.200 [skip ci]

--- check-dotnet-core.ps1
+++ check-dotnet-core.ps1
@@ -48,7 +48,7 @@ if($env:isVs2017 -ne 'true') {
 $netCoreSDK = (Get-ChildItem 'C:\Program Files\dotnet\sdk')
 if($env:isVs2017 -ne 'true') {
   # VS 2015
-  if($netCoreSDK.Length -ne 14) { throw 'Wrong number of .NET Core SDKs installed' }
+  if($netCoreSDK.Length -ne 15) { throw 'Wrong number of .NET Core SDKs installed' }
   if($netCoreSDK[0].Name -ne '1.0.0-preview2-003121') { throw '.NET Core SDK 1.0.0-preview2-003121 not found'}
   if($netCoreSDK[1].Name -ne '1.0.0-preview2-003131') { throw '.NET Core SDK 1.0.0-preview2-003131 not found'}
   if($netCoreSDK[2].Name -ne '1.0.0-preview2-003156') { throw '.NET Core SDK 1.0.0-preview2-003156 not found'}
@@ -61,10 +61,11 @@ if($env:isVs2017 -ne 'true') {
   if($netCoreSDK[9].Name -ne '2.1.103') { throw '.NET Core SDK 2.1.103 not found'}
   if($netCoreSDK[10].Name -ne '2.1.104') { throw '.NET Core SDK 2.1.104 not found'}
   if($netCoreSDK[11].Name -ne '2.1.105') { throw '.NET Core SDK 2.1.105 not found'}
-  if($netCoreSDK[12].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
+  if($netCoreSDK[12].Name -ne '2.1.200') { throw '.NET Core SDK 2.1.200 not found'}
+  if($netCoreSDK[13].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
 } else {
   # VS 2017
-  if($netCoreSDK.Length -ne 16) { throw 'Wrong number of .NET Core SDKs installed' }
+  if($netCoreSDK.Length -ne 17) { throw 'Wrong number of .NET Core SDKs installed' }
   if($netCoreSDK[0].Name -ne '1.0.1') { throw '.NET Core SDK 1.0.1 not found'}
   if($netCoreSDK[1].Name -ne '1.0.2') { throw '.NET Core SDK 1.0.2 not found'}
   if($netCoreSDK[2].Name -ne '1.0.3') { throw '.NET Core SDK 1.0.3 not found'}
@@ -78,6 +79,7 @@ if($env:isVs2017 -ne 'true') {
   if($netCoreSDK[10].Name -ne '2.1.103') { throw '.NET Core SDK 2.1.103 not found'}
   if($netCoreSDK[11].Name -ne '2.1.104') { throw '.NET Core SDK 2.1.104 not found'}
   if($netCoreSDK[12].Name -ne '2.1.105') { throw '.NET Core SDK 2.1.105 not found'}
-  if($netCoreSDK[13].Name -ne '2.1.2') { throw '.NET Core SDK 2.1.2 not found'}
-  if($netCoreSDK[14].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
+  if($netCoreSDK[13].Name -ne '2.1.200') { throw '.NET Core SDK 2.1.200 not found'}
+  if($netCoreSDK[14].Name -ne '2.1.2') { throw '.NET Core SDK 2.1.2 not found'}
+  if($netCoreSDK[15].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
 }
