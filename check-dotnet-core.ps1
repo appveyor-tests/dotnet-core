1503ab4399f322e18f90f95fe386036216ce426d
Feodor Fitsner <feodor@appveyor.com>
2018-06-01 18:39:25 -0700

    SDK 2.1.300 added [skip ci]

--- check-dotnet-core.ps1
+++ check-dotnet-core.ps1
@@ -17,7 +17,7 @@ $netCoreRuntime = (Get-ChildItem 'C:\Program Files\dotnet\shared\Microsoft.NETCo
 
 if($env:isVs2017 -ne 'true') {
   # VS 2015
-  if($netCoreRuntime.Length -ne 13) { throw 'Wrong number of .NET Core runtimes installed' }
+  if($netCoreRuntime.Length -ne 14) { throw 'Wrong number of .NET Core runtimes installed' }
   if($netCoreRuntime[0].Name -ne '1.0.0') { throw '.NET Core 1.0.0 not found'}
   if($netCoreRuntime[1].Name -ne '1.0.1') { throw '.NET Core 1.0.1 not found'}
   if($netCoreRuntime[2].Name -ne '1.0.3') { throw '.NET Core 1.0.3 not found'}
@@ -31,9 +31,10 @@ if($env:isVs2017 -ne 'true') {
   if($netCoreRuntime[10].Name -ne '2.0.5') { throw '.NET Core 2.0.5 not found'}
   if($netCoreRuntime[11].Name -ne '2.0.6') { throw '.NET Core 2.0.6 not found'}
   if($netCoreRuntime[12].Name -ne '2.0.7') { throw '.NET Core 2.0.7 not found'}
+  if($netCoreRuntime[13].Name -ne '2.1.0') { throw '.NET Core 2.1.0 not found'}
 } else {
   # VS 2017
-  if($netCoreRuntime.Length -ne 9) { throw 'Wrong number of .NET Core runtimes installed' }
+  if($netCoreRuntime.Length -ne 10) { throw 'Wrong number of .NET Core runtimes installed' }
   if($netCoreRuntime[0].Name -ne '1.0.4') { throw '.NET Core 1.0.4 not found'}
   if($netCoreRuntime[1].Name -ne '1.0.5') { throw '.NET Core 1.0.5 not found'}
   if($netCoreRuntime[2].Name -ne '1.1.1') { throw '.NET Core 1.1.1 not found'}
@@ -43,12 +44,13 @@ if($env:isVs2017 -ne 'true') {
   if($netCoreRuntime[6].Name -ne '2.0.5') { throw '.NET Core 2.0.5 not found'}
   if($netCoreRuntime[7].Name -ne '2.0.6') { throw '.NET Core 2.0.6 not found'}
   if($netCoreRuntime[8].Name -ne '2.0.7') { throw '.NET Core 2.0.7 not found'}
+  if($netCoreRuntime[9].Name -ne '2.1.0') { throw '.NET Core 2.1.0 not found'}
 }
 
 $netCoreSDK = (Get-ChildItem 'C:\Program Files\dotnet\sdk')
 if($env:isVs2017 -ne 'true') {
   # VS 2015
-  if($netCoreSDK.Length -ne 15) { throw 'Wrong number of .NET Core SDKs installed' }
+  if($netCoreSDK.Length -ne 16) { throw 'Wrong number of .NET Core SDKs installed' }
   if($netCoreSDK[0].Name -ne '1.0.0-preview2-003121') { throw '.NET Core SDK 1.0.0-preview2-003121 not found'}
   if($netCoreSDK[1].Name -ne '1.0.0-preview2-003131') { throw '.NET Core SDK 1.0.0-preview2-003131 not found'}
   if($netCoreSDK[2].Name -ne '1.0.0-preview2-003156') { throw '.NET Core SDK 1.0.0-preview2-003156 not found'}
@@ -62,10 +64,11 @@ if($env:isVs2017 -ne 'true') {
   if($netCoreSDK[10].Name -ne '2.1.104') { throw '.NET Core SDK 2.1.104 not found'}
   if($netCoreSDK[11].Name -ne '2.1.105') { throw '.NET Core SDK 2.1.105 not found'}
   if($netCoreSDK[12].Name -ne '2.1.200') { throw '.NET Core SDK 2.1.200 not found'}
-  if($netCoreSDK[13].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
+  if($netCoreSDK[13].Name -ne '2.1.300') { throw '.NET Core SDK 2.1.300 not found'}
+  if($netCoreSDK[14].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
 } else {
   # VS 2017
-  if($netCoreSDK.Length -ne 17) { throw 'Wrong number of .NET Core SDKs installed' }
+  if($netCoreSDK.Length -ne 18) { throw 'Wrong number of .NET Core SDKs installed' }
   if($netCoreSDK[0].Name -ne '1.0.1') { throw '.NET Core SDK 1.0.1 not found'}
   if($netCoreSDK[1].Name -ne '1.0.2') { throw '.NET Core SDK 1.0.2 not found'}
   if($netCoreSDK[2].Name -ne '1.0.3') { throw '.NET Core SDK 1.0.3 not found'}
@@ -81,5 +84,6 @@ if($env:isVs2017 -ne 'true') {
   if($netCoreSDK[12].Name -ne '2.1.105') { throw '.NET Core SDK 2.1.105 not found'}
   if($netCoreSDK[13].Name -ne '2.1.2') { throw '.NET Core SDK 2.1.2 not found'}
   if($netCoreSDK[14].Name -ne '2.1.200') { throw '.NET Core SDK 2.1.200 not found'}
-  if($netCoreSDK[15].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
+  if($netCoreSDK[15].Name -ne '2.1.300') { throw '.NET Core SDK 2.1.300 not found'}
+  if($netCoreSDK[16].Name -ne '2.1.4') { throw '.NET Core SDK 2.1.4 not found'}
 }
