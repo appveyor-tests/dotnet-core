a0107e91467e26b20c6811f838d9ca72fab2a076
Feodor Fitsner <feodor@appveyor.com>
2018-09-14 08:15:30 -0700

    SDK 2.1.402 added

--- check-dotnet-core.ps1
+++ check-dotnet-core.ps1
@@ -28,6 +28,7 @@ $dotnet_runtimes = @{
   '2.1.1' = @{ vs2015 = $true; vs2017 = $true }
   '2.1.2' = @{ vs2015 = $true; vs2017 = $true }
   '2.1.3' = @{ vs2015 = $true; vs2017 = $true }
+  '2.1.4' = @{ vs2015 = $true; vs2017 = $true }
 }
 
 $dotnet_sdks = @{
@@ -57,6 +58,7 @@ $dotnet_sdks = @{
   '2.1.302' = @{ vs2015 = $true; vs2017 = $true }
   '2.1.400' = @{ vs2015 = $true; vs2017 = $true }
   '2.1.401' = @{ vs2015 = $true; vs2017 = $true }
+  '2.1.402' = @{ vs2015 = $true; vs2017 = $true }
 }
 
 # check runtimes
