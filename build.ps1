77a2322a6d1d6b88fae07b7c3028a02d1b8145c9
Alena Hrynkevich <alena@appveyor.com>
2019-08-05 14:23:44 -0700

    Created build.ps1

---
+++ build.ps1
@@ -0,0 +1,7 @@
+$env:isVs2017 = 'false'
+if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $env:isVs2017 = 'true' }
+$result = (cmd /c dotnet new console)
+$result
+if ($result[1] -eq 'Welcome to .NET Core!') { throw "Welcome to .NET Core message found!" }
+.\check-dotnet-core.ps1
+test.cmd
