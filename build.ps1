587c2c43c6fcfa891c829deb079a3eb6769c01f6
Feodor Fitsner <feodor@appveyor.com>
2020-04-19 11:45:17 -0700

    Delete build.ps1

--- build.ps1
+++
@@ -1,9 +0,0 @@
-$env:dotnetcore_version = '2.2.301'
-$env:isVs2017 = 'false'
-if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $env:isVs2017 = 'true' }
-$result = (cmd /c dotnet new console)
-$result
-if ($result[1] -eq 'Welcome to .NET Core!') { throw "Welcome to .NET Core message found!" }
-.\check-dotnet-core.ps1
-cmd.exe /c "test.cmd"
-if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
