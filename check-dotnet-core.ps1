5f425f3a0ba5b3a0abdeca932a70686c0395f1f3
Feodor Fitsner <feodor@appveyor.com>
2020-04-24 13:53:17 -0700

    Fix .NET SDK versions for VS 2019 image

--- check-dotnet-core.ps1
+++ check-dotnet-core.ps1
@@ -6,9 +6,10 @@ if($core_version -ne $env:dotnetcore_version) { throw "Expected .NET Core: $env:
 
 $dotnet_sdks = @(
   '1.1.14'
-  '2.1.803'
+  '2.1.202'
+  '2.1.805'
   '2.2.402'
-  '3.0.102'
+  '3.0.103'
   '3.1.201'
 )
 
